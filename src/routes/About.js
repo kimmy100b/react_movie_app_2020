10bc0aa36289e67ac067370540ccfb02aa771ca0
kimmy100b <kimmy100b@gmail.com>
2020-09-07 16:05:25 +0900

    post : Sharing Props Between Routes

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function About() {
-  return <span>About this page: I built it</span>;
+  return (
+    <div className="about__container">
+      <span>“Freedom is the freedom to say that two plus two make four. If that is granted, all else follows.”</span>
+      <span>− George Orwell, 1984</span>
+    </div>
+  );
 }
 
 export default About;
