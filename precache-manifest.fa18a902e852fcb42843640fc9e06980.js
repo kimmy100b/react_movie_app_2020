77f6278640d54b5e1682393ce61139e3737b77ff
kimmy100b <kimmy100b@gmail.com>
2020-09-07 14:07:16 +0900

    Updates

---
+++ precache-manifest.fa18a902e852fcb42843640fc9e06980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6ad8bbdb157d2d3a22d995d259bec6",
+    "url": "/react_movie_app_2020/index.html"
+  },
+  {
+    "revision": "6b37a348b42920c43856",
+    "url": "/react_movie_app_2020/static/css/main.b91c88b1.chunk.css"
+  },
+  {
+    "revision": "83c0753fd9db99cbca91",
+    "url": "/react_movie_app_2020/static/js/2.8e13a63e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app_2020/static/js/2.8e13a63e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b37a348b42920c43856",
+    "url": "/react_movie_app_2020/static/js/main.6b6c5ae0.chunk.js"
+  },
+  {
+    "revision": "4dee63d2173baae61e6f",
+    "url": "/react_movie_app_2020/static/js/runtime-main.7e96a2c7.js"
+  }
+]);
